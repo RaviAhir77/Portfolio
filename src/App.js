f0e5d1eba2b6b90f79d74b31ca87834f9991161b
RaviAhir77 <ravigaiya70@gmail.com>
2025-01-15 22:50:51 +0530

    skill page commit first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import Profile from './view/Profile'
+import Skill from './view/Skill';
+
 
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <Skill/>
     </div>
   );
 }
